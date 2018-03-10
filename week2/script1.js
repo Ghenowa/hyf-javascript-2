0f2a56045539bb02aa7461a740a90dbfbc5a9e0d
Ghenowa <35230172+Ghenowa@users.noreply.github.com>
2018-03-10 14:54:56 +0100

    Update script1.js

--- week2/script1.js
+++ week2/script1.js
@@ -1,3 +1,19 @@
+//step1
+
+/*let numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 !== 0) {
+        newNumbers.push(numbers[i] * 2);
+    }
+}
+
+console.log("The doubled numbers are", newNumbers); // [2, 6]
+
+*/
+
+
 /*
 var numbers=[1,2,3,4];
 var newNumbers=[];
@@ -13,5 +29,24 @@ var numbers=[1,2,3,4];
 var newNumbers=[];
 newNumbers=numbers.filter(x=> x%2 !==0).map(x=> x*2);
 console.log(newNumbers);
+
+//step2
+
+/*let numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 !== 0) {
+        newNumbers.push(numbers[i] * 2);
+    }
+}
+
+console.log("The doubled numbers are", newNumbers); // [2, 6]
+
+*/
+
+let tasks=mondy.concat(tuesday);
+
+
 
 
